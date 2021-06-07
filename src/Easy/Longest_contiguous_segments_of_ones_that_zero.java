7a3d8b599e34d1f719a59447e7f1374ad08d663f
Manish Sharma <omsharma571222@gmail.com>
2021-06-07 20:56:29 +0530

    Longest_contiguous_segments_of_ones_that_zero

--- src/Easy/Longest_contiguous_segments_of_ones_that_zero.java
+++ src/Easy/Longest_contiguous_segments_of_ones_that_zero.java
@@ -1,5 +1,23 @@
 package Easy;
 
 public class Longest_contiguous_segments_of_ones_that_zero {
+    public static void checkZeroOnes(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            int count1=1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count1++;
+                } else {
+                    i=j;
+                    System.out.println(s.charAt(i));
+                    break;
+                }
+            }
+            System.out.println(count1);
+        }
+    }
 
+    public static void main(String[] args) {
+        checkZeroOnes("11001");
+    }
 }
